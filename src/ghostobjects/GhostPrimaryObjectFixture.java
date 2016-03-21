965894531282c535e27a7653eafb2e3c5123dffa
Shaibal Chakraborty <shaibalchakraborty@shaibals-MacBook-Pro.local>
2016-03-21 14:39:18 +1300

    wait for page to load

--- src/ghostobjects/GhostPrimaryObjectFixture.java
+++ src/ghostobjects/GhostPrimaryObjectFixture.java
@@ -4,12 +4,14 @@ import helper.configuration.AutoConfig;
 import helper.driver.Driver;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 
 public class GhostPrimaryObjectFixture {
 
 	protected void userLogin(){
 	Driver.get().navigate().to("http://localhost:2368/login"); 
+	checkPageIsReady();
 	WebElement username = Driver.get().findElement(By.xpath("//input[contains(@type,'email')]"));
 	username.sendKeys(AutoConfig.getInstance().getUserName());
 	WebElement password = Driver.get().findElement(By.xpath("//input[contains(@type,'password')]"));
@@ -18,4 +20,28 @@ public class GhostPrimaryObjectFixture {
 	signin.click();
 
 }
-}
+	public void checkPageIsReady() {
+		  
+		  JavascriptExecutor js = (JavascriptExecutor)Driver.get();
+		  
+		  
+		  //Initially bellow given if condition will check ready state of page.
+		  if (js.executeScript("return document.readyState").toString().equals("complete")){ 
+		   System.out.println("Page Is loaded.");
+		   return; 
+		  } 
+		  
+		  //This loop will rotate for 25 times to check If page Is ready after every 1 second.
+		  //You can replace your value with 25 If you wants to Increase or decrease wait time.
+		  for (int i=0; i<25; i++){ 
+		   try {
+		    Thread.sleep(1000);
+		    }catch (InterruptedException e) {} 
+		   //To check page ready state.
+		   if (js.executeScript("return document.readyState").toString().equals("complete")){ 
+		    break; 
+		   }   
+		}
+	  }
+	}
+
